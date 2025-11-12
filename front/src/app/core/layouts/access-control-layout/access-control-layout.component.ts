5f5622d195568d727a9d341a2ede6239317e9571
Leyton M. Espitia Diaz <leyton_espitia@hotmail.com>
2025-11-11 22:16:15 -0500

    feat(layout): reestructurar y mover componentes de diseño de acceso

--- front/src/app/core/layouts/access-control-layout/access-control-layout.component.ts
+++ front/src/app/core/layouts/access-control-layout/access-control-layout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '../../components/header/header.component';
-import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { HeaderComponent } from '../../../modules/access-control/components/header/header.component';
+import { SidebarComponent } from '../../../modules/access-control/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-access-control-layout',
